d8cc0416c489f6a5fbe77f8fb0f799cd63b523fb
William Clifford <bcliffor@systemsbiology.org>
2018-05-09 15:41:08 -0700

    wget needs a new token

--- build/install_clamav.sh
+++ build/install_clamav.sh
@@ -4,7 +4,8 @@ set -x
 
 ## Install clamav                                                                                                                     
 sudo apt install -y clamav clamav-daemon                                                                                           
-wget https://raw.githubusercontent.com/isb-cgc/ISB-CGC-Cron/master/gce_vm_tasks/virus_scan.sh?token=AKBQyIdXio073v7hMOxWHnXCCp#Dqxgl7ks5alcxdwA%3D%3D -O virus_scan
+wget https://raw.githubusercontent.com/isb-cgc/ISB-CGC-Cron/master/gce_vm_tasks/virus_scan.sh?token=AZWqo20xg5GkQJdIYtVRo_O5GZmZezWEks5a_LKtwA%3D%3D -O virus_scan
+
 chmod 0755 virus_scan                                                                                                              
 sudo cp virus_scan /etc/cron.daily/                                                                                                
 sudo sed -ie 's/Checks 24/Checks 2/' freshclam.conf                                                                                
