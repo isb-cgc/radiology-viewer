5879ed3409dd45c72f8c111671fb57eaa25e0d80
William Clifford <bcliffor@systemsbiology.org>
2018-05-09 15:59:23 -0700

    Need fill path to freshclam.conf

--- build/install_clamav.sh
+++ build/install_clamav.sh
@@ -6,6 +6,8 @@ set -x
 sudo apt install -y clamav clamav-daemon                                                                                           
 wget https://raw.githubusercontent.com/isb-cgc/ISB-CGC-Cron/master/gce_vm_tasks/virus_scan.sh?token=AZWqo20xg5GkQJdIYtVRo_O5GZmZezWEks5a_LKtwA%3D%3D -O virus_scan
 
-chmod 0755 virus_scan                                                                                                              
-sudo cp virus_scan /etc/cron.daily/                                                                                                
-sudo sed -ie 's/Checks 24/Checks 2/' freshclam.conf                                                                                
+chmod 0755 virus_scan
+
+sudo cp virus_scan /etc/cron.daily/ 
+
+sudo sed -ie 's/Checks 24/Checks 2/' /etc/clamav/freshclam.conf
