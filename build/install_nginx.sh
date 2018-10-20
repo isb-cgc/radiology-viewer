d6fe0fd0aed31d49907d48468cfb0883b4a2414b
William Clifford <bcliffor@systemsbiology.org>
2018-10-20 17:07:31 +0000

    Further tailor letsencrypt config to only strong ciphers

--- build/install_nginx.sh
+++ build/install_nginx.sh
@@ -5,14 +5,14 @@ set -x
 CONFIG_BUCKET=$1
 MACHINE_URL=$2
 
-# First install nginx                                                                                         
+# First install nginx
 sudo apt-get install -y nginx
 
-# Replace default config and insert domain name of this VM                                                    
+# Replace default config and insert domain name of this VM
 sudo cp ./nginx/nginx.conf /etc/nginx/nginx.conf
 sudo sed -ie "s/SERVER_NAME/$MACHINE_URL/" /etc/nginx/nginx.conf
 
-# Now install certbot                                                                                         
+# Now install certbot                                              
 #sudo rm -rf /etc/letsencrypt
 
 sudo apt-get update
@@ -21,18 +21,19 @@ sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install -y python-certbot-nginx
 
-# Get the admin email address                                                                                 
+# Get the admin email address                                                               
 sudo gsutil cp gs://$CONFIG_BUCKET/ir_addr.txt .
 SERVER_ADMIN=`cat ir_addr.txt`
 sudo rm ir_addr.txt
 
-# Ccreate a new cert. Note that Let's Encrypt strictly limits creating new certs on the                
-# same domain to 10 in a one week period.                                                                     
+# Ccreate a new cert. Note that Let's Encrypt strictly limits creating new certs on the
+# same domain to 10 in a one week period.                       
 sudo certbot --nginx -m $SERVER_ADMIN -d $MACHINE_URL --redirect --agree-tos --non-interactive --staging
 
-# Edit the letsencrypt config file so as to not enable TLS v1.0                                               
+# Edit the letsencrypt config file so as to not enable TLS v1.0 and only use some strong ciphers     
 sudo sed -ie 's/TLSv1 / /' /etc/letsencrypt/options-ssl-nginx.conf
+sudo sed -ie 's/^ssl_ciphers.*/ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";/' /etc/letsencrypt/options-ssl-nginx.conf
 
-# Restart nginx                                                                                               
+# Restart nginx                                            
 sudo nginx -s stop
 sudo nginx
