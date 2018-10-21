2504c09dabcf25d60296ff23d67a3d93a72468b9
William Clifford <bcliffor@systemsbiology.org>
2018-10-21 21:34:29 +0000

    Don't pass unused params

--- startup.sh
+++ startup.sh
@@ -2,21 +2,15 @@
 # This is script is called after the initial reboot of a camic-viewer VM.
 # We want to execute run_viewer.sh as the same user that instantiated the VM.
 
-# $1=VIEWER_VERSION
-# $2=SERVER_ADMIN
-# $3=SERVER_NAME
-# $4=SERVER_ALIAS
-# $5=WEBAPP
-
 set -x
 
 PROGNAME=$(basename "$0")
 
-if [ "$#" -ne 5 ]; then
+if [ "$#" -ne 0 ]; then
     echo "Usage: ./$PROGNAME <quip-viewer version> <admin email> <ip addr> <server alias> <webapp>"
     exit 1;
 fi
 
 cd /home/dvproc/radiology-viewer
 
-./run_viewer.sh $1 $2 $3 $4 $5
+./run_viewer.sh 
