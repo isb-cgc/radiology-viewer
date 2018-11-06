45109d60e1e5c199195406175b5dfe0fe8c0f5b4
William Clifford <bcliffor@systemsbiology.org>
2018-11-06 10:45:47 -0800

    Get a real cert

--- build/install_nginx.sh
+++ build/install_nginx.sh
@@ -43,8 +43,8 @@ else
 
     # Create a new cert. Note that Let's Encrypt strictly limits creating new certs on the
     # same domain to 10 in a one week period. 
-    sudo certbot --nginx -m $SERVER_ADMIN -d $MACHINE_URL --redirect --agree-tos --non-interactive --staging
-    #sudo certbot --nginx -m $SERVER_ADMIN -d $MACHINE_URL --redirect --agree-tos --non-interactive
+    #sudo certbot --nginx -m $SERVER_ADMIN -d $MACHINE_URL --redirect --agree-tos --non-interactive --staging
+    sudo certbot --nginx -m $SERVER_ADMIN -d $MACHINE_URL --redirect --agree-tos --non-interactive
 
     # Edit the letsencrypt config file so as to not enable TLS v1.0 and only use some strong ciphers     
     sudo sed -ie 's/TLSv1 / /' /etc/letsencrypt/options-ssl-nginx.conf
